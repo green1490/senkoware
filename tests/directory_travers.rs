d08251e3eb9a8b9577317a81ca62ee68d4fd8d5e
green1490 <zambokrisztian1490@gmail.com>
2024-12-31 17:38:35 +0100

    test cases

---
+++ tests/directory_travers.rs
@@ -0,0 +1,31 @@
+use std::collections::VecDeque;
+
+struct Node {
+    child:Vec<Node>,
+    name:String,
+    is_file:bool
+}
+/* 
+              N (root)
+            / | \
+(file.txt) N  N  N (etc, dev)
+                 |
+                 N (test.conf)
+*/
+#[test]
+fn directory_discovery() -> Result<(), std::io::Error> {
+    let root = Node {name: "Root".to_string(), is_file: false, child: vec![
+        Node{name: "file.txt".to_string(), is_file: true, child: vec![]},
+        Node{name: "etc".to_string(), is_file: false, child: vec![]},
+        Node{name: "dev".to_string(), is_file: false, child: vec![
+            Node{name: "test.conf".to_string(), is_file: true, child: vec![]},
+        ]}
+    ]};
+    let mut stack:VecDeque<Node> = VecDeque::from(root.child);
+    while stack.len() != 0 {
+        let current_node = stack.pop_front().unwrap();
+        stack.extend(current_node.child);
+        println!("{}", current_node.name);
+    }
+    Ok(())
+}
---
+++ tests/key_transfer.rs
@@ -0,0 +1,11 @@
+use std::{io::{Read, Write}, net::{TcpListener, TcpStream}};
+
+#[test]
+fn tcp_write() -> Result<(), std::io::Error> {
+    const IP:&str = "127.0.0.1:4444";
+    let mut buffer:Vec<u8> = vec![];
+    let mut streamer = TcpStream::connect(IP)?;
+    let _ = streamer.write("Transfering the key...".as_bytes());
+
+    Ok(())
+}
