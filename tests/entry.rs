d9c9282005c3d481b484f9fda8587e79a88fe5a3
green1490 <zambokrisztian1490@gmail.com>
2025-01-12 22:04:25 +0100

    filter test case

---
+++ tests/entry.rs
@@ -0,0 +1,31 @@
+use std::path::Path;
+
+// filters by extension
+#[test]
+fn entry_filter() {
+    let gitignore:Vec<&str> = vec!["node_modules", "filter.txt"];
+    
+    let entries:Vec<&str> = vec![
+        "/home",
+        "flag.txt",
+        "/path/to/flag.txt",
+        "node_moduels",
+        "filter.txt"
+    ];
+    // checks if it has extension
+    // and if its not in the gitignore file
+    let filtered_entries:Vec<&&str> = entries.iter().filter(
+        |entry| {
+            if gitignore.contains(&entry) {
+                return false;
+            }
+            else if Path::new(entry).extension().is_none() {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+    ).collect();
+    assert_eq!(2, filtered_entries.len());
+}
